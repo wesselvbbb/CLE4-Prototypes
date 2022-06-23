4aff0061582ecc47bf565e5ee1294f469c67f9fa
Wessel <1023694@hr.nl>
2022-06-23 14:37:54 +0200

    added audio prototype

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # CLE4-Prototypes
 <a href="./player/docs/index.html">Player</a>
 <a href="./evolution/docs/index.html">Evolution</a>
+<a href="./audio/docs/index.html">Audio</a>
---
+++ audio/src/Smog.ts
@@ -0,0 +1,58 @@
+import * as PIXI from 'pixi.js'
+import { Player } from './Player'
+
+
+export class Smog extends PIXI.Graphics{
+
+    player : Player
+    radius:number
+    originalRadius:number
+    
+
+
+    constructor(player: Player, radius:number){
+        super()
+        this.player = player
+        this.originalRadius = radius
+        this.radius = this.originalRadius
+        //this.interactive = true
+        this.draw()
+        
+        
+    }
+
+    draw() {
+        this.beginFill(0xffffff)
+        this.alpha = 0.3
+        this.drawCircle(this.player.x, this.player.y,this.radius)        
+        this.endFill
+
+    }
+
+    updatePos() {
+       
+    }
+
+    reset() {
+        this.radius = this.originalRadius
+            this.clear()
+            this.draw()
+    }
+
+    update(){
+        if(this.radius >= 1) {
+        this.radius -= 1
+        this.clear()
+        this.draw()
+        //console.log(`radius: ${this.radius}`)
+        }
+        else{
+            //console.log(`radius is already 0`)
+            //console.log('resetting circle to 200 radius')
+            this.reset()
+        }
+    }
+
+
+
+}
